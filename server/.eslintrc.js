753536745ee0557f3910358aa703e9f11ab50350
Nehrkorn <anehrkorn@NEHAL-AMBP.fritz.box>
2020-09-13 20:45:00 +0200

    Initial server code

---
+++ server/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:vue/essential"],
+  plugins: ["vue"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
